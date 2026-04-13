6aeccbff61c727289b7a8abc540ff166081028f8
sthalin 2026 <sthalin.rivera.2026@gmail.com>
2026-04-13 11:36:42 -0500

    add module message

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import deviceRoutes from "./routes/device.routes.js";
 import otaRoutes from "./routes/ota.routes.js"; // 🔥 NUEVO
-
+import messageRoutes from "./routes/message.routes.js";
 const app = express();
 
 app.use(cors());
@@ -10,5 +10,5 @@ app.use(express.json());
 
 app.use("/api/device", deviceRoutes);
 app.use("/api/ota", otaRoutes); // 🔥 IMPORTANTE
-
+app.use("/api/message", messageRoutes);
 export default app;
