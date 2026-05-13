8028c22f1f72d4967cf5661a8ba284387ac61bd1
sthalin <sthalin.11@gmail.com>
2026-05-13 18:46:44 -0500

    add root route

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import app from "./app.js";
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Servidor corriendo en puerto ${PORT}`);
