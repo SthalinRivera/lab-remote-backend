213ab798214ebc96411773ef5da7d6a5ae69ccb8
sthalin <sthalin.11@gmail.com>
2026-05-13 21:05:01 -0500

    add root route

--- src/server.js
+++ src/server.js
@@ -1,7 +1,18 @@
 import "dotenv/config";
 import app from "./app.js";
+import bucket from "./firebase.js";
+import "./services/worker.js";
 
 const PORT = process.env.PORT || 3000;
+
+(async () => {
+  try {
+    const [files] = await bucket.getFiles();
+    console.log("🔥 Firebase conectado");
+  } catch (error) {
+    console.error("❌ Error Firebase:", error);
+  }
+})();
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Servidor corriendo en puerto ${PORT}`);
